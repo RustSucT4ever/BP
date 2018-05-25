1f6e27436ad4c02ffef7d67b6616286cdf5ec00f
IljaMacht <mogutnovil@hotmail.com>
2018-05-25 10:46:19 +0200

    pseudocode for lib and cargo dependencies

--- RustSucT4ever_save_load_BP_lib/src/lib.rs
+++ RustSucT4ever_save_load_BP_lib/src/lib.rs
@@ -1,3 +1,16 @@
+fn load_BP(file_path: &string) -> (&string){
+    // datei lesen
+    // deserialisieren
+    // überprüfen ob das geladene auch ein BP ist
+    // ausgeben
+}
+
+fn save_BP(&string) -> (file_path: &string){
+    // serialisieren
+    // datei speichern
+}
+
+
 #[cfg(test)]
 mod tests {
     #[test]
