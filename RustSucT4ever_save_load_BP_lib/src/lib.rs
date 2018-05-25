1a03c9c2f9621069a5ffa41c3b4a9d172e1a79c1
Mustafa Yalciner <mustafayalciner@outlook.de>
2018-05-25 11:23:35 +0200

    Bring methods to be tested into scope.

--- RustSucT4ever_save_load_BP_lib/src/lib.rs
+++ RustSucT4ever_save_load_BP_lib/src/lib.rs
@@ -15,11 +15,13 @@ fn save_bp(tree: &String) -> String{
 
 #[cfg(test)]
 mod tests {
+    use load_bp;
+    use save_bp;
     #[test]
     fn load_loads_the_same_that_has_been_saved() {
         let example = String::from("1100");
-        let example_path = String::from(save_BP(&example));
-        let checksum = String::from(load_BP(example_path));
+        let example_path = String::from(save_bp(&example));
+        let checksum = String::from(load_bp(&example_path));
         assert_eq!(checksum, example);
     }
 }
