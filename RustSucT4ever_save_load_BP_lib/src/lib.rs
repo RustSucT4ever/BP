7538406009c8c21756b43fd98103768a54dcf8bc
Mustafa Yalciner <mustafayalciner@outlook.de>
2018-06-01 10:31:37 +0200

    Implement load and persist functions.

--- RustSucT4ever_save_load_BP_lib/src/lib.rs
+++ RustSucT4ever_save_load_BP_lib/src/lib.rs
@@ -10,25 +10,25 @@ static SOME_BV_TREE: &'static str =
 use std::error::Error;
 use std::path::Path;
 
-fn load_bp(file_path: &String) -> String {
+fn load_bp(file_path: &String) -> BitVec {
     let mut f = File::open(&file_path).expect("file not found");
     let mut contents = String::new();
     f.read_to_string(&mut contents)
         .expect("something went wrong reading the file");
-    //let mut bitVec: BitVec = serde::de::
-
+    
+    let mut bitVec: BitVec = serde_json::from_str(&contents).unwrap();
+    return bitVec;
     // datei lesen
     // deserialisieren
     // überprüfen ob das geladene auch ein BP ist
     // ausgeben
-    return String::from("");
 }
 
-fn save_bp(tree: &String) -> String{ 
+fn save_bp(tree: &BitVec) -> String{ 
     // serialisieren
     //
     // datei speichern
-    let path = Path::new("out/our_bv_tree.txt");
+    let path = Path::new("our_bv_tree.txt");
     let display = path.display();
 
     // Open a file in write-only mode, returns `io::Result<File>`
@@ -38,16 +38,17 @@ fn save_bp(tree: &String) -> String{
                            why.description()),
         Ok(file) => file,
     };
-
+    
+    let bv_tree_str : String = serde_json::to_string(tree).unwrap();
     // Write the `LOREM_IPSUM` string to `file`, returns `io::Result<()>`
-    match file.write_all(SOME_BV_TREE.as_bytes()) {
+    match file.write_all(bv_tree_str.as_bytes()) {
         Err(why) => {
             panic!("couldn't write to {}: {}", display,
                                                why.description())
         },
         Ok(_) => println!("successfully wrote to {}", display),
     }
-    return String::from ("")
+    return String::from ("path------")
 }
 
 #[cfg(test)]
