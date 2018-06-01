1abb33a6a2c4ca2a19099679b6d3dc9feb42daf9
IljaMacht <mogutnovil@hotmail.com>
2018-06-01 11:01:22 +0200

    test works!

--- RustSucT4ever_save_load_BP_lib/src/lib.rs
+++ RustSucT4ever_save_load_BP_lib/src/lib.rs
@@ -48,22 +48,23 @@ fn save_bp(tree: &BitVec) -> String{
         },
         Ok(_) => println!("successfully wrote to {}", display),
     }
-    return String::from ("path------")
+    return display.to_string();
 }
 
 #[cfg(test)]
 mod tests {
     use load_bp;
     use save_bp;
+    use bv::{BitVec};
     #[test]
     fn load_loads_the_same_that_has_been_saved() {
-        let mut example: BitVec = BitVec::new();
+        let mut example = BitVec::new();
         example.push(true);
         example.push(true);
         example.push(false);
         example.push(false);
         let example_path = String::from(save_bp(&example));
-        let checksum = String::from(load_bp(&example_path));
+        let checksum = load_bp(&example_path);
         assert_eq!(checksum, example);
     }
 }
