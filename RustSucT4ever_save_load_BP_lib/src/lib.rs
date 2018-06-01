9dd30ce16421a126bf72378db853411904e67ab1
IljaMacht <mogutnovil@hotmail.com>
2018-06-01 11:21:23 +0200

    check single bits during the test

--- RustSucT4ever_save_load_BP_lib/src/lib.rs
+++ RustSucT4ever_save_load_BP_lib/src/lib.rs
@@ -57,12 +57,14 @@ fn save_bp(tree: &BitVec) -> String{
 mod tests {
     use load_bp;
     use save_bp;
-    use bv::{BitVec};
+    use bv::{BitVec, Bits};
     #[test]
     fn load_loads_the_same_that_has_been_saved() {
         // create an example BV tree
         let mut example = BitVec::new();
         example.push(true);
+        example.push(true);
+        example.push(false);
         example.push(true);
         example.push(false);
         example.push(false);
@@ -75,5 +77,11 @@ mod tests {
 
         // be happy!
         assert_eq!(checksum, example);
+        assert_eq!(checksum.get_bit(0), true);
+        assert_eq!(checksum.get_bit(1), true);
+        assert_eq!(checksum.get_bit(2), false);
+        assert_eq!(checksum.get_bit(3), true);
+        assert_eq!(checksum.get_bit(4), false);
+        assert_eq!(checksum.get_bit(5), false);
     }
 }
