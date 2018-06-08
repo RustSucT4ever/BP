497b0f2e83908833a90464e9eb1c0222d3b56ff6
Mustafa Yalciner <mustafayalciner@outlook.de>
2018-06-08 09:22:41 +0200

    Remove common directory.

--- RustSucT4ever_save_load_BP_lib/src/Bp/mod.rs
+++ RustSucT4ever_save_load_BP_lib/src/Bp/mod.rs
@@ -7,6 +7,13 @@ use std::io::prelude::*;
 use std::error::Error;
 use std::path::Path;
 
+fn load_file(file_path: &String) -> String{
+    let mut f = File::open(&file_path).expect("file not found");    
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+    return contents;
+}
 
 pub fn load_bp(file_path: &String) -> BitVec {
     // datei lesen
