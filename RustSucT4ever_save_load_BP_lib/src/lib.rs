79e7f79f8c2e8a8423059141e53458bdbff75670
Mustafa Yalciner <mustafayalciner@outlook.de>
2018-06-08 11:55:26 +0200

    Create function to read from a manually create file and test it.

--- RustSucT4ever_save_load_BP_lib/src/lib.rs
+++ RustSucT4ever_save_load_BP_lib/src/lib.rs
@@ -1,4 +1,7 @@
 extern crate bv;
+use bv::{BitVec, Bits};
+use std::fs::File;
+use std::io::prelude::*;
 mod bp;
 mod louds;
 
@@ -9,11 +12,31 @@ trait BpLoudsCommonTrait {
     fn next_sibling(pos:u64) -> u64; 
 }
 
+fn load_from_manual_file(file_path:String) -> BitVec{
+    // datei lesen
+    let contents = load_file(&file_path);
+    let mut loaded_vec = BitVec::new();
+    for i in 0..contents.len(){
+        let to_push = contents.chars().nth(i)==Some('(')||contents.chars().nth(i)==Some('1');
+        loaded_vec.push(to_push);
+    }
+    return loaded_vec
+}
+ 
+fn load_file(file_path: &String) -> String {
+    let mut f = File::open(&file_path).expect("file not found");
+    let mut contents = String::new();
+    f.read_to_string(&mut contents)
+        .expect("something went wrong reading the file");
+    return contents;
+}
+
 #[cfg(test)]
 mod tests {
     use bp::*;
     use louds::*;
     use bv::{BitVec, Bits};
+    use load_from_manual_file;
 
     #[test]
     fn check_bp_save_and_load() {
@@ -57,7 +80,6 @@ mod tests {
 
         // save BV to file
         let mut example_path = String::from(save_louds(&example));
-
         // load BV from file
         let mut checksum = load_louds(&example_path);
 
@@ -71,5 +93,15 @@ mod tests {
         assert_eq!(checksum.get_bit(5), false);
         assert_eq!(checksum.get_bit(6), false);
         assert_eq!(checksum.get_bit(7), false);
+    }
+    #[test]
+    fn loads_from_manual_file(){
+        let bit_vec = load_from_manual_file("manualTree.txt".to_owned());
+        assert_eq!(bit_vec.get_bit(0), true);
+        assert_eq!(bit_vec.get_bit(1), true);
+        assert_eq!(bit_vec.get_bit(2), false);
+        assert_eq!(bit_vec.get_bit(3), true);
+        assert_eq!(bit_vec.get_bit(4), false);
+        assert_eq!(bit_vec.get_bit(5), false);
     }
 }
