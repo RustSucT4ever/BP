30e26709db70ee3f3292d55c35e049bada99d8b3
IljaMacht <mogutnovil@hotmail.com>
2018-06-22 10:43:17 +0200

    all louds tests work

--- RustSucT4ever_save_load_BP_lib/src/Louds.rs
+++ RustSucT4ever_save_load_BP_lib/src/Louds.rs
@@ -11,14 +11,25 @@ struct Louds {
 
 impl Louds {
     fn degree(&self, x: u64) -> u64{
-        self.next_0(x) - x
+        self.next_0(x-1) - x
     }
     fn child(&self,x: u64, i: u64) -> u64{
         self.dataStruct.select_0(self.dataStruct.rank_1(x).unwrap()+i-2).unwrap() + 1
     }
     fn child_rank (&self, x: u64) -> u64{
-        let y = self.dataStruct.select_1(self.dataStruct.rank_0(x-1).unwrap()).unwrap();
-        y - self.prev_0(y)
+        if x == 1 {
+            return 0
+        }
+        else {
+            let y = self.dataStruct.select_1(self.dataStruct.rank_0(x-1).unwrap()).unwrap() + 1;
+            let z = self.prev_0(y);
+            if z > 0 {        
+                y - z
+            }
+            else {
+                y - 1
+            }
+        }
     }
     pub fn new(bits: BitVec<u8>) -> Louds{
         let n = bits.bit_len();
@@ -65,19 +76,23 @@ mod tests {
     #[test]
     fn test_is_leaf(){
         let test_tree : Louds = create_test_tree();
-        assert!(test_tree.isleaf(3));
+        assert!(test_tree.isleaf(4));
+        assert!(test_tree.isleaf(7));
     } 
 
     #[test]
     fn test_parent(){
         let test_tree : Louds = create_test_tree();
-        assert_eq!(test_tree.parent(3),1);
+        assert_eq!(test_tree.parent(4),1);
+        assert_eq!(test_tree.parent(5),1);
+        assert_eq!(test_tree.parent(7),5);
     }
 
     #[test]
     fn test_first_child(){
         let test_tree : Louds = create_test_tree();
         assert_eq!(test_tree.first_child(1),4);
+        assert_eq!(test_tree.first_child(5),7);
     }
 
     #[test]
@@ -89,8 +104,28 @@ mod tests {
     #[test]
     fn test_degree(){
         let test_tree : Louds = create_test_tree();
+        assert_eq!(test_tree.degree(1),2);
         assert_eq!(test_tree.degree(4),0);
+        assert_eq!(test_tree.degree(5),1);
+        assert_eq!(test_tree.degree(7),0);
     }  
+
+    #[test]
+    fn test_child(){
+        let test_tree : Louds = create_test_tree();
+        assert_eq!(test_tree.child(1,1),4);
+        assert_eq!(test_tree.child(1,2),5);
+        assert_eq!(test_tree.child(5,1),7);
+    } 
+
+    #[test]
+    fn test_child_rank(){
+        let test_tree : Louds = create_test_tree();
+        assert_eq!(test_tree.child_rank(1),0);
+        assert_eq!(test_tree.child_rank(4),0);
+        assert_eq!(test_tree.child_rank(5),1);
+        assert_eq!(test_tree.child_rank(7),0);
+    } 
 
     fn create_test_tree() -> Louds {
         let mut louds_tree = BitVec::new();
