c3fa515a48e9368ed5ce04249899b7a4e467deb7
Mustafa Yalciner <mustafayalciner@outlook.de>
2018-07-10 11:07:44 +0200

    Implement BP functions and uncomment the test methods.

--- RustSucT4ever_save_load_BP_lib/src/bp.rs
+++ RustSucT4ever_save_load_BP_lib/src/bp.rs
@@ -7,49 +7,53 @@ use std::fs::File;
 use std::io::prelude::*;
 use std::error::Error;
 use std::path::Path;
+use RangeMinMax::RangeMinMax;
 
 struct Bp {
-    bitString: BitVec<u8>,
+    tree: RangeMinMax
 }
-/*
+
 impl BpLoudsCommonTrait for Bp {
     fn isleaf (&self, pos:u64) -> bool{
-        return false
+       return self.tree.fwdsearch(pos, 0)==pos+1;
     }
     fn parent(& self, pos:u64) -> u64{
-        return 1
+        return self.tree.bwdsearch(pos, -2)+1;
     }
-    fn first_child(&self, pos:u64) -> u64{
-        return 1
+    fn first_child(&self, pos:u64) -> u64{  //TODO turn return value to an optional for the case that a child does not exist.
+        return pos+1;
     }
-    fn next_sibling(&self, pos:u64) -> u64{
-        return 1
+    fn next_sibling(&self, pos:u64) -> u64{ //TODO incase next sibling does not exist.
+        return self.tree.fwdsearch(pos, -1)+1
     }
 }
 
 impl Bp {
-    pub fn new(bits: BitVec<u8>) -> Bp{
-        Bp {bitString: bits}
+    pub fn new(bits: BitVec<u8>, block_size: u64) -> Bp{
+        Bp {tree: RangeMinMax::new(bits, block_size)}
     }
 
     pub fn pre_rank(&self, pos:u64) -> u64 {
-        return 1;
+       return  self.tree.rmm_rank_one(pos);
     }
 
     pub fn pre_select(&self, pos:u64) -> u64 {
-        return 1;
+        return self.tree.rmm_select_one(pos);
     }
 
     pub fn depth(&self, pos:u64) -> u64 {
-        return 1;
+        return self.tree.calc_excess(pos);
+    }
+    pub fn find_close(&self, pos:u64) -> u64{
+        return self.tree.fwdsearch(pos,-1);
     }
 
-    pub fn ancestor(&self, pos_x:u64,pos_y:u64) -> u64 {
-        return 1;
+    pub fn ancestor(&self, pos_x:u64,pos_y:u64) -> bool {
+        return pos_x<=pos_y && pos_y<=self.find_close(pos_x);
     }
 
     pub fn subtree_size(&self, pos:u64) -> u64 {
-        return 1;
+        return (self.find_close(pos)-pos+1)/2;
     }
     
     pub fn load_file(file_path: &String) -> String{
@@ -60,6 +64,7 @@ impl Bp {
         return contents;
     }
 
+/*
     pub fn load_bp(file_path: &String) -> BitVec {
         // datei lesen
         let mut contents = load_file(&file_path);
@@ -92,7 +97,7 @@ impl Bp {
         // ausgeben
         return bit_vec;
     }
-
+*/
     pub fn save_bp(tree: &BitVec) -> String{
         // define where to store file
         let path = Path::new("our_bv_tree.txt");
@@ -121,16 +126,16 @@ impl Bp {
     }
     
 }
-*/
-/*
+
+
 #[cfg(test)]
 mod tests {
     use bv::{BitVec, Bits};
-    use bp::*;
+    use bp;
 
     #[test]
     fn test_pre_rank(){
-        let test_tree : BitVec = create_test_tree();
+        let test_tree = create_test_tree();
         assert_eq!(test_tree.pre_rank(0), 1);
         assert_eq!(test_tree.pre_rank(1), 2);
         assert_eq!(test_tree.pre_rank(2), 2);
@@ -142,7 +147,7 @@ mod tests {
 
     #[test]
     fn test_pre_select(){
-        let test_tree : BitVec = create_test_tree();
+        let test_tree = create_test_tree();
         assert_eq!(test_tree.pre_select(1), 0);
         assert_eq!(test_tree.pre_select(2), 1);
         assert_eq!(test_tree.pre_select(3), 3);
@@ -150,7 +155,7 @@ mod tests {
 
     #[test]
     fn test_ancestor(){
-        let test_tree : BitVec = create_test_tree();
+        let test_tree = create_test_tree();
         assert!(test_tree.ancestor(0,3));
         assert!(test_tree.ancestor(0,1));
         assert!(!test_tree.ancestor(1,3));
@@ -158,7 +163,7 @@ mod tests {
 
     #[test]
     fn test_subtree_size(){
-        let test_tree : BitVec = create_test_tree();
+        let test_tree = create_test_tree();
         assert_eq!(test_tree.subtree_size(0), 3);
         assert_eq!(test_tree.subtree_size(1), 1);
         assert_eq!(test_tree.subtree_size(3), 1);
@@ -166,7 +171,7 @@ mod tests {
 
     #[test]
     fn test_depth(){
-        let test_tree : BitVec = create_test_tree();
+        let test_tree = create_test_tree();
         assert_eq!(test_tree.depth(0), 1);
         assert_eq!(test_tree.depth(1), 2);
         assert_eq!(test_tree.depth(3), 3);
@@ -174,32 +179,32 @@ mod tests {
 
     #[test]
     fn test_isleaf(){
-        let test_tree : BitVec = create_test_tree();
-        assert!(test_tree.!isleaf(0));
+        let test_tree = create_test_tree();
+        assert!(!test_tree.isleaf(0));
         assert!(test_tree.isleaf(1));
         assert!(test_tree.isleaf(3));
     }
 
     #[test]
     fn test_parent(){
-        let test_tree : BitVec = create_test_tree();
+        let test_tree = create_test_tree();
         assert_eq!(test_tree.parent(1), 0);
         assert_eq!(test_tree.parent(3), 0);
     }
 
     #[test]
     fn test_first_child(){
-        let test_tree : BitVec = create_test_tree();
+        let test_tree = create_test_tree();
         assert_eq!(test_tree.first_child(0), 1);
     }
 
     #[test]
     fn test_next_sibling(){
-        let test_tree : BitVec = create_test_tree();
+        let test_tree = create_test_tree();
         assert_eq!(test_tree.next_sibling(1), 3);
     }
 
-    fn create_test_tree() -> Bp {
+    fn create_test_tree() -> bp {
         let mut bp_tree = BitVec::new();
             bp_tree.push(true);
             bp_tree.push(true);
@@ -211,4 +216,3 @@ mod tests {
         return bp_example;
     }
 }
-*/
