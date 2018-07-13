c66984c0b03b72526f32f8f5e3dc4bd341b56e9f
IljaMacht <mogutnovil@hotmail.com>
2018-07-13 10:14:08 +0200

    old lib folder removed

--- RustSucT4ever_save_load_BP_lib/src/common_tree.rs
+++
@@ -1,6 +0,0 @@
-pub trait BpLoudsCommonTrait {
-    fn isleaf (&self,pos:u64) -> bool;
-    fn parent(&self,pos:u64) -> u64;
-    fn first_child(&self,pos:u64) -> Option<u64>;
-    fn next_sibling(&self,pos:u64) -> Option<u64>; 
-}
